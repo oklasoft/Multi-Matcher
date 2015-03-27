05949a9c18ec4e9fbd72905f98cb793e1a34c2a6
Stuart Glenn <glennsb@magni-4.omrf.hsc.net.ou.edu>
2015-03-27 14:25:45 -0500

    Initial Commit

---
+++ Multi Matcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Multi Matcher
+//
+//  Created by Stuart Glenn on 2015-03-27.
+//  Copyright (c) 2015 Oklahoma Medical Research Foundation. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
