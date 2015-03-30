e5d8aaa278a48dd34a4d6f69b078be93e3273b84
Stuart Glenn <glennsb@magni-4.omrf.hsc.net.ou.edu>
2015-03-30 14:29:52 -0500

    Add actions & outlets for file selection portions of GUI

--- Multi Matcher/AppDelegate.swift
+++ Multi Matcher/AppDelegate.swift
@@ -12,6 +12,10 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var caseFileField: NSTextField!
+    @IBOutlet weak var controlFileField: NSTextField!
+    @IBOutlet weak var outputFileField: NSTextField!
+    @IBOutlet weak var runButton: NSButton!
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
@@ -22,6 +26,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func doSelectCases(sender: AnyObject) {
+        selectFileDialog("Choose", message: "Select CSV file containing cases", target: caseFileField)
+    }
+    @IBAction func doSelectControls(sender: AnyObject) {
+        selectFileDialog("Choose", message: "Select CSV file containing controls", target: controlFileField)
+    }
+    
+    @IBAction func doSelectOutput(sender: AnyObject) {
+        var panel = NSSavePanel()
+        panel.message = "Select destination file to save matches"
+        panel.allowedFileTypes = ["csv"]
+        panel.beginSheetModalForWindow(window) {
+            (result : Int) in
+            if NSFileHandlingPanelOKButton == result {
+                var theURL = panel.URL
+                NSLog("Selected %@", theURL!.path!)
+                self.outputFileField.stringValue = theURL!.path!
+            }
+        }
 
+    }
+    func selectFileDialog(prompt: String, message: String, target: NSTextField) {
+        var panel = NSOpenPanel()
+        
+        panel.prompt = prompt
+        panel.message = message
+        panel.allowedFileTypes = ["csv"]
+        panel.canChooseDirectories = false
+        panel.canChooseFiles = true
+        panel.resolvesAliases = true
+        panel.allowsMultipleSelection = false
+        
+        panel.beginSheetModalForWindow(window) {
+            (result : Int) in
+            if NSFileHandlingPanelOKButton == result {
+                var theURL = panel.URL
+                NSLog("Selected %@", theURL!.path!)
+                target.stringValue = theURL!.path!
+            }
+        }
+    }
+
+    @IBAction func runMatch(sender: AnyObject) {
+    }
 }
 
