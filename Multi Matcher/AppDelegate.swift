f01045f99ee9420c13778d0a3ed81532ae4e267c
Stuart Glenn <glennsb@magni-3.local>
2015-04-03 18:40:51 -0500

    Pointless rearrangement

--- Multi Matcher/AppDelegate.swift
+++ Multi Matcher/AppDelegate.swift
@@ -3,13 +3,18 @@
 //  Multi Matcher
 //
 //  Created by Stuart Glenn on 2015-03-27.
-//  Copyright (c) 2015 Oklahoma Medical Research Foundation. All rights reserved.
+//  Copyright (c) 2015 Stuart Glenn, Oklahoma Medical Research Foundation. 
+//  All rights reserved.
+//  Use of this source code is governed by a 3 clause BSD style license.
+//  Full details can be found in the LICENSE file distributed with this software
 //
 
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+
+    let mmatcherPath = NSBundle.mainBundle().pathForResource("mmatcher", ofType: "x86_64")
 
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var caseFileField: NSTextField!
@@ -20,28 +25,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var statusSpinner: NSProgressIndicator!
     @IBOutlet weak var runButton: NSButton!
 
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-    
-    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
-        return true
-    }
-    
-
+    //Select an input file for the A file (cases)
     @IBAction func doSelectCases(sender: AnyObject) {
         selectFileDialog("Choose", message: "Select CSV file containing cases", target: caseFileField)
     }
     
+    //Select an input for the B file (controls)
     @IBAction func doSelectControls(sender: AnyObject) {
         selectFileDialog("Choose", message: "Select CSV file containing controls", target: controlFileField)
     }
     
+    //Select save destination
     @IBAction func doSelectOutput(sender: AnyObject) {
         var panel = NSSavePanel()
         panel.message = "Select destination file to save matches"
@@ -54,8 +48,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 self.enableRunIfReady()
             }
         }
-
+        
     }
+    
+    //Fire of the actual mmatcher run
+    @IBAction func runMatch(sender: AnyObject) {
+        results.string = ""
+        if !checkReadyToRun() {
+            results.string = "Not enough parametersgopkg.in/alecthomas/kingpin.v1"
+            return
+        }
+        statusSpinner.startAnimation(self)
+        runButton.title = "Stop"
+        results.string = "Running... \(mmatcherPath!)"
+        statusSpinner.stopAnimation(self)
+    }
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    //Delegation for input text fields to enable/disable Run button
+    override func controlTextDidChange(obj: NSNotification) {
+        enableRunIfReady()
+    }
+    
+    //Prompt for a CSV file
     func selectFileDialog(prompt: String, message: String, target: NSTextField) {
         var panel = NSOpenPanel()
         
@@ -77,6 +101,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
+    //We can run if there are two input files, a selection of column & an output filename
     func checkReadyToRun() -> Bool {
         return caseFileField.stringValue != "" && controlFileField.stringValue != "" &&
             outputFileField.stringValue != "" && keyField.stringValue != ""
@@ -86,20 +111,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         runButton.enabled = checkReadyToRun()
     }
     
-    @IBAction func runMatch(sender: AnyObject) {
-        results.string = ""
-        if !checkReadyToRun() {
-            results.string = "Not enough parametersgopkg.in/alecthomas/kingpin.v1"
-            return
-        }
-        statusSpinner.startAnimation(self)
-        runButton.title = "Stop"
-        results.string = "Running..."
-        statusSpinner.stopAnimation(self)
-    }
-    
-    override func controlTextDidChange(obj: NSNotification) {
-        enableRunIfReady()
-    }
 }
 
